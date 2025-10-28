6974c782d1a4c5ac0d3411c5a6aa03f8905edf3f
mangoBIG <115971358+mangoBIG@users.noreply.github.com>
2025-10-28 12:15:38 +0700

    commit2

--- index.php
+++ index.php
@@ -3,4 +3,6 @@
 
 echo "<h1> my name is Mango </h1>";
 echo "<h2> ip 66017992 </h2>";
+echo "<h3> mango is big dog </h3>";
+
 ?>
---
+++ menu.php
@@ -0,0 +1,5 @@
+<?php 
+   echo "<h1> menu  </h1>";
+   echo "<h2> big black banana </h2>";
+
+   ?>
